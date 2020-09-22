7a02e842236180eeaa2dc66a4596499b8b309fd6
陈光临 <chenguanglin@bishijie.com>
2020-09-22 18:59:15 +0800

    Initial commit

---
+++ Example/GLWebViewController/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  GLWebViewController
+//
+//  Created by findm on 09/22/2020.
+//  Copyright (c) 2020 findm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
