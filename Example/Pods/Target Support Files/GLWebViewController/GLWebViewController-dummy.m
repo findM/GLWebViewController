7f3779771a95e5fd6500a41ee26de4e50ebdb27a
陈光临 <chenguanglin@bishijie.com>
2020-09-22 19:07:32 +0800

    init

---
+++ Example/Pods/Target Support Files/GLWebViewController/GLWebViewController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GLWebViewController : NSObject
+@end
+@implementation PodsDummy_GLWebViewController
+@end
---
+++ Example/Pods/Target Support Files/Pods-GLWebViewController_Tests/Pods-GLWebViewController_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GLWebViewController_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_GLWebViewController_Tests
+@end
